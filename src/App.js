20949d9cc6235387d3cc9e4fa80b6b682dea6e33
CTLT-D3410-2\Administrator <ctv3.bcn1.e@ctgroupvietnam.com>
2022-06-04 10:29:21 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  const [count, setCount] = useState(60);
+
+  const timerId = useRef();
+  const prevCount = useRef();
+
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
+
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    clearInterval(timerId.current);
+  };
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
