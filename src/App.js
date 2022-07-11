bce482608ae5cca71a0128a160ad30b228111e28
CTLT-D3410-2\Administrator <ctv3.bcn1.e@ctgroupvietnam.com>
2022-07-11 10:19:55 +0700

    save stage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+// 1. memo() => Higher Order Component (HOC)
+// 2. useCallback()
+
 function App() {
-  return <div className="App">App</div>;
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <div className="App">{count}</div>
+      <button onClick={increase}>Increase</button>
+    </>
+  );
 }
 
 export default App;
