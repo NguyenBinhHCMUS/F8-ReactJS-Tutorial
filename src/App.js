ea5ef1186f9c90c4fb05e0f6ba5863498e28037d
CTLT-D3410-2\Administrator <ctv3.bcn1.e@ctgroupvietnam.com>
2022-07-12 14:35:43 +0700

    custom hook use call api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  return <div className="App">App</div>;
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useFetch("https://reqres.in/api/users");
+
+  if (error) {
+    return "Something wrong!!!";
+  }
+  return isLoading ? (
+    <p>Loading ...</p>
+  ) : (
+    users.map((user) => (
+      <p>
+        {user.first_name} {users.last_name}
+      </p>
+    ))
+  );
 }
 
 export default App;
